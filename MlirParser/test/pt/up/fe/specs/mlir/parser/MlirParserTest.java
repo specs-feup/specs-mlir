f09bd69abb7a9b14c6c5b419397ff980a7013058
João Bispo <joaobispo@gmail.com>
2021-06-18 23:44:13 +0100

    [MlirParser] Adds project

---
+++ MlirParser/test/pt/up/fe/specs/mlir/parser/MlirParserTest.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2021 SPeCS.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License. under the License.
+ */
+
+package pt.up.fe.specs.mlir.parser;
+
+import org.junit.Test;
+
+import pt.up.fe.specs.util.SpecsIo;
+import pt.up.fe.specs.util.SpecsLogs;
+import pt.up.fe.specs.util.utilities.TestResources;
+
+public class MlirParserTest {
+
+    private static final TestResources TEST_RESOURCES = new TestResources("pt/up/fe/specs/mlir/parser");
+
+    @Test
+    public void test() {
+        var resource = TEST_RESOURCES.getResource("llvm_intrinsic.mlir");
+        var parser = new MlirParser(SpecsIo.resourceToStream(resource.getResource()));
+        try {
+            var root = parser.Root();
+            root.dump(" ");
+        } catch (ParseException e) {
+            SpecsLogs.warn("Error message:\n", e);
+        }
+    }
+
+}
