4b2ae1013c1694e5ce9bc37fc2f27a09b41441c9
João Bispo <jbispo@fe.up.pt>
2023-07-26 13:58:47 +0100

    [MlirAntlrToMlirAst] Adds method 'parse()'

--- MlirAntlrToMlirAst/src/pt/up/fe/specs/mlir/antlr2nodedata/AntlrToMlirNodeConverter.java
+++ MlirAntlrToMlirAst/src/pt/up/fe/specs/mlir/antlr2nodedata/AntlrToMlirNodeConverter.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RuleContext;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -27,8 +29,9 @@ import pt.up.fe.specs.mlir.ast.context.MlirContext;
 import pt.up.fe.specs.mlir.ast.dummy.Root;
 import pt.up.fe.specs.mlir.ast.operation.GenericOperation;
 import pt.up.fe.specs.mlir.ast.operation.Operation;
+import pt.up.fe.specs.mlir.grammar.MlirLexer;
+import pt.up.fe.specs.mlir.grammar.MlirParser;
 import pt.up.fe.specs.mlir.grammar.MlirParser.GenericOperationContext;
-import pt.up.fe.specs.mlir.grammar.MlirParser.Op_resultContext;
 import pt.up.fe.specs.mlir.grammar.MlirParser.OperationContext;
 import pt.up.fe.specs.mlir.grammar.MlirParser.RootContext;
 import pt.up.fe.specs.util.classmap.BiFunctionClassMap;
@@ -51,6 +54,29 @@ public class AntlrToMlirNodeConverter {
         this.converterData = new ConverterData(parser, new MlirContext());
     }
 
+    public static MlirNode parse(String mlirCode) {
+        // Convert code string into a character stream
+        var input = new ANTLRInputStream(mlirCode);
+        // Transform characters into tokens using the lexer
+        var lex = new MlirLexer(input); // Will we interact with this?
+
+        // Wrap lexer around a token stream
+        var tokens = new CommonTokenStream(lex);
+        // Transforms tokens into a parse tree
+        var parser = new MlirParser(tokens);
+
+        var rootNode = parser.root();
+
+        // System.out.println(rootNode.toStringTree());
+
+        var converter = new AntlrToMlirNodeConverter(parser);
+        var mlirNodeRoot = converter.convert(rootNode);
+
+        // System.out.println(mlirNodeRoot.toTree());
+
+        return mlirNodeRoot;
+    }
+
     public MlirNode convert(ParseTree node) {
         // Apply converter from map
         var mlirNode = CONVERTERS.apply(node, converterData);
@@ -62,11 +88,11 @@ public class AntlrToMlirNodeConverter {
         var children = new ArrayList<MlirNode>();
         for (int i = 0; i < node.getChildCount(); i++) {
             var childAntlrNode = node.getChild(i);
-            
-            if(childAntlrNode instanceof TerminalNode) {
-            	continue;
+
+            if (childAntlrNode instanceof TerminalNode) {
+                continue;
             }
-            
+
             var childMlirNode = convert(childAntlrNode);
             children.add(childMlirNode);
         }
@@ -81,30 +107,28 @@ public class AntlrToMlirNodeConverter {
         return data.getContext().getFactory().root(root.getClass().getSimpleName());
     }
 
-//    private static Root convertOperation(OperationContext op, ConverterData data) {
-//        return data.getContext().getFactory().root(op.getClass().getSimpleName());
-//    }
-//    
-    
+    // private static Root convertOperation(OperationContext op, ConverterData data) {
+    // return data.getContext().getFactory().root(op.getClass().getSimpleName());
+    // }
+    //
+
     private static Operation newOperation(OperationContext op, ConverterData data) {
-//    	System.out.println("asd" + op.getChild(Op_resultContext.class, 0).getText());
-    	
-    	return data.getContext().getFactory().operation("RESULT");
+        // System.out.println("asd" + op.getChild(Op_resultContext.class, 0).getText());
+
+        return data.getContext().getFactory().operation("RESULT");
 
     }
-    
+
     private static GenericOperation convertGenericOperation(GenericOperationContext genOp, ConverterData data) {
-    	var op = newOperation(genOp, data);
+        var op = newOperation(genOp, data);
         var genOpNode = data.getContext().getFactory().genericOperation(genOp.name.getText());
         genOpNode.getData().addAll(op);
-        
+
         return genOpNode;
     }
-    
+
     private static Root convertDefault(ParseTree node, ConverterData data) {
         return data.getContext().getFactory().root("Default for " + node.getClass());
     }
-    
-    
-    
+
 }
